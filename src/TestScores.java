1101d95a1c5cb4ef195d408399a1a8bf1b853f2f
clapp.lauren <clapp.lauren@SEMS-259-10.SEMS259.LAB>
2018-04-18 14:53:26 -0700

    calculator scores ip

---
+++ src/TestScores.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class TestScores {
+public static void main(String[] args) {
+	
+	String answer = JOptionPane.showInputDialog("What is your test score?");
+	double testscore = Double.parseDouble(answer);
+	if (testscore >= 90.00 );
+	
+}
+}
