2209fb1820812bcfd3e7bfa654786cb96f6463be
clapp.lauren <clapp.lauren@SEMS-259-10.SEMS259.LAB>
2018-04-25 14:57:02 -0700

    string methods pialoud ip

--- src/PiAloud.java
+++ src/PiAloud.java
@@ -8,11 +8,16 @@ import java.util.Scanner;
 public class PiAloud {
 
 	// 1. Make a main method and make sure your program can run
+	public static void main(String[] args) {
+		
+	}
 
 	// 2. Make a String variable to hold the value of Pi. You could use http://www.piday.org/million/ for the value.
+	String pi = "3.141592653";
 
 	// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
-
+	system.out.println(pi.charAt(i));
+	
 	// 4. Print ALL the digits of of Pi (hint: use a loop)
 
 	// 5. Use the speak() method to speak all the digits of Pi.
